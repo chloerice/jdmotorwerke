d527745372c1b5e01bcf212a63fa6a7d7a26e418
Chloe Rice <chloe.mi.rice@gmail.com>
2017-07-26 12:01:43 -0700

    firebase setup

--- firebase.js
+++ firebase.js
@@ -1,14 +1,28 @@
-const firebase = require('firebase')
+const firebase = require('firebase/app')
+require('firebase/auth')
+require('firebase/database')
+require('firebase/storage')
 // inside the firebase console, create a new project
 // then you'll be able to copy your config object by simply clicking the
-// "Add Firebase to your web app" button on the Overview screen at
+// 'Add Firebase to your web app' button on the Overview screen at
 // https://console.firebase.google.com/project/YOUR_PROJECT_ID/overview
 const config = {
-  apiKey: 'YOUR_API_KEY_HERE',
-  authDomain: 'YOUR_AUTH_DOMAIN_HERE',
-  databaseURL: 'YOUR_DB_URL_HERE',
-  projectId: 'YOUR_PROJECT_ID_HERE',
-  storageBucket: 'YOUR_STORAGE_BUCKET_HERE'
+  apiKey: 'AIzaSyBxN9hLOuoq8MU5dQFVETu1CxRjItjZjGc',
+  authDomain: 'jdmotorwerke.firebaseapp.com',
+  databaseURL: 'https://jdmotorwerke.firebaseio.com',
+  projectId: 'jdmotorwerke',
+  storageBucket: 'jdmotorwerke.appspot.com',
+  messagingSenderId: '590261283764'
 }
 
 firebase.initializeApp(config)
+const storage = firebase.storage()
+const database = firebase.database()
+const auth = firebase.auth()
+
+module.exports = {
+  auth,
+  storage,
+  database,
+  firebase
+}
