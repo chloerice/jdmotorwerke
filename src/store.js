e3fe4e75a8b306166a79f7bb7034372beb3c1cb5
Chloe Rice <chloe.mi.rice@gmail.com>
2018-03-19 03:03:46 -0700

    fixed middleware, fixed image placeholder on mobile

--- src/store.js
+++ src/store.js
@@ -3,17 +3,11 @@ import rootReducer from './reducers'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 
-let loggingMiddleware = null
+let middleware = [thunkMiddleware]
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  loggingMiddleware = createLogger({collapsed: true})
+  middleware.push(createLogger({collapsed: true}))
 }
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    loggingMiddleware,
-    thunkMiddleware
-  )
-)
+const store = createStore(rootReducer, applyMiddleware(...middleware))
 
 export default store
